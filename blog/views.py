0946b0a72d470ef8cea2d3d53334d14c7eda1116
Zahira <zahiimanzur@gmail.com>
2018-03-12 15:01:54 -0300

    post_list

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
